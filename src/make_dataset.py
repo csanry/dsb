de33a8da1ccbadff8a65c1de93b0a643f07b0d51
csanry <clarencesan@gmail.com>
2022-05-19 13:09:11 +0800

    update make_dataset.py

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -1,7 +1,58 @@
-def main() -> None:
-    pass
+import logging
+import os
+import zipfile
+
+from src import config
+
+os.environ["KAGGLE_USERNAME"] = "clarencesry"
+os.environ["KAGGLE_KEY"] = "006967cfc619633984f9a17e5d8538f0"
+
+
+def make_dataset():
+    log_fmt = "%(asctime)s:%(name)s:%(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    from kaggle.api.kaggle_api_extended import KaggleApi
+
+    logger = logging.getLogger()
+
+    api = KaggleApi()
+    api.authenticate()
+
+    file_names = [
+        "olist_customers_dataset.csv",
+        "olist_geolocation_dataset.csv",
+        "olist_order_items_dataset.csv",
+        "olist_order_payments_dataset.csv",
+        "olist_order_reviews_dataset.csv",
+        "olist_orders_dataset.csv",
+        "olist_products_dataset.csv",
+        "olist_sellers_dataset.csv",
+        "product_category_name_translation.csv",
+    ]
+
+    logger.info("Downloading files")
+    for file in file_names:
+        api.dataset_download_file(
+            dataset="olistbr/brazilian-ecommerce",
+            file_name=file,
+            path=config.RAW_FILE_PATH,
+        )
+
+    logger.info("Extracting zip files")
+    for file in os.listdir(config.RAW_FILE_PATH):
+        if file.endswith(".zip"):
+            with zipfile.ZipFile(config.RAW_FILE_PATH / file, "r") as zipref:
+                zipref.extractall(path=config.RAW_FILE_PATH)
+                os.remove(config.RAW_FILE_PATH / file)
+
+    logger.info("\n".join(os.listdir(config.RAW_FILE_PATH)))
+
+    logger.info("Done")
+
+
+def main():
+    make_dataset()
 
 
 if __name__ == "__main__":
     main()
-
